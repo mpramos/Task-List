aa68a907010060b86ef337f21bf85ecdff0e9dba
mpramos <mpramos78915400@gmail.com>
2022-05-30 17:34:42 -0400

    Persistencia de datos con Local Storage

--- src/App/index.js
+++ src/App/index.js
@@ -1,14 +1,26 @@
 import React from "react";
 import { AppUI } from "./AppUI";
-const defaultTodos = [
-  { text: "Cortar cebolla 1", completed: false },
-  { text: "llorar con la llorona", completed: true },
-  { text: "ir al museo", completed: false },
-  { text: "lavar ropa", completed: true },
-];
+// const defaultTodos = [
+//   { text: "Cortar cebolla 1", completed: false },
+//   { text: "llorar con la llorona", completed: true },
+//   { text: "ir al museo", completed: false },
+//   { text: "lavar ropa", completed: true },
+// ];
 function App() {
-  const [todos, setTodos] = React.useState(defaultTodos);
+  const localStorageTodos=localStorage.getItem('TODOS_V1') ;
+  // *? Creamos en primer array por si aun el usuario no ha creado nada
+  let parsedTodos;
+  // *? Si es que ese localStorage ya existe entonces traeremos la informacion
+  if(!localStorageTodos){ //*? Si No existe nada en el local Storage
+    localStorage.setItem('TODOS_V1',JSON.stringify([]));
+    parsedTodos=[];
+  } else{
+    parsedTodos=JSON.parse(localStorageTodos)
+  }
+
+  const [todos, setTodos] = React.useState(parsedTodos);
   const [searchValue, setSearchValue] = React.useState("");
+  
   // *? contando TODOs
   const completedTodos = todos.filter((todo) => !!todo.completed).length;
   const totalTodos = todos.length;
@@ -27,6 +39,14 @@ function App() {
       return todoText.includes(searchText);
     });
   }
+  //*? Desde aqui hacemos persistencia de datos para la lista de tareas
+  //*? La funcion saveTodos servirá como puente para localStorage y completeTodo
+  const saveTodos=(newTodos)=>{
+    const stringifiedTodos=JSON.stringify(newTodos);
+    localStorage.setItem('TODOS_V1', stringifiedTodos);
+    setTodos(newTodos)
+  }
+
   const completeTodo = (text) => {
     //*? Extraemos la posicion de la tarea que queremos eliminar para poder editarlo
     const todoIndex = todos.findIndex((todo) => todo.text === text);
@@ -38,13 +58,13 @@ function App() {
     // *? Insertamos todos los todos a la nueva lista newTodos
     const newTodos = [...todos];
     newTodos[todoIndex].completed = true;
-    setTodos(newTodos);
+    saveTodos(newTodos);
   };
   const deleteTodo = (text) => {
     const todoIndex = todos.findIndex((todo) => todo.text === text);
     const newTodos = [...todos];
     newTodos.splice(todoIndex, 1);
-    setTodos(newTodos);
+    saveTodos(newTodos);
   };
   return (
     <AppUI
